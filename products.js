185fd14e6b0c46ea471a3510bcd51f556aa2d164
hasin-shabbir <66528929+hasin-shabbir@users.noreply.github.com>
2020-11-08 14:35:12 +0400

    bug fix

--- products.js
+++ products.js
@@ -1,15 +1,24 @@
 $('.slider-for').slick({
  slidesToShow: 1,
  slidesToScroll: 1,
+ variableWidth: true,
  arrows: false,
+ initialSlide: 1,
+ mobileFirst: true,
  // fade: true,
- asNavFor: '.slider-nav'
+ asNavFor: '.slider-nav',
+
+
 });
+
 $('.slider-nav').slick({
  slidesToShow: 3,
  slidesToScroll: 1,
  asNavFor: '.slider-for',
  dots: true,
+ initialSlide: 1,
+ mobileFirst: true,
  centerMode: true,
- focusOnSelect: true
+ focusOnSelect: true,
+
 });
