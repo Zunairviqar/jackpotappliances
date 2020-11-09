e0c7f59ed05878b2b2fa605173aef47877262b0b
hasin-shabbir <66528929+hasin-shabbir@users.noreply.github.com>
2020-11-09 11:38:23 +0400

    contact form linked

--- index.js
+++ index.js
@@ -18,7 +18,12 @@ $('.owl-carousel').owlCarousel({
 $(document).on('click', '.dropdown-menu', function (e) {
   e.stopPropagation();
 });
-
+// var cat1=document.getElementById('cat1');
+// var cat2=document.getElementById('cat2');
+// var cat3=document.getElementById('cat3');
+// var sub1=document.getElementById('sub1');
+// var sub2=document.getElementById('sub2');
+// var sub3=document.getElementById('sub3');
 // make it as accordion for smaller screens
 if ($(window).width() < 992) {
   $('.dropdown-menu a').click(function(e){
@@ -30,4 +35,20 @@ if ($(window).width() < 992) {
      $(this).find('.submenu').hide();
   })
   });
+  // cat1.addEventListener("click",function(){
+  //   console.log("clicked");
+  //   if (sub1.style.display!=="block"){
+  //     sub1.style.display="block";
+  //   }else{sub1.style.display="none";}
+  // });
+  // cat2.addEventListener("click",function(){
+  //   if (sub2.style.display!=="block"){
+  //     sub2.style.display="block";
+  //   }else{sub2.style.display="none";}
+  // });
+  // cat3.addEventListener("click",function(){
+  //   if (sub3.style.display!=="block"){
+  //     sub3.style.display="block";
+  //   }else{sub3.style.display="none";}
+  // });
 }
