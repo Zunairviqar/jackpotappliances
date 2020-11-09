340b4f73c8445c58b0a5ba70f7fe9303c93418c4
Zunairviqar <66274735+Zunairviqar@users.noreply.github.com>
2020-11-09 22:02:33 +0400

    updatessss

--- products.js
+++ products.js
@@ -22,3 +22,34 @@ $('.slider-nav').slick({
  focusOnSelect: true,
 
 });
+
+var detailButtons=[];
+document.querySelectorAll('.detailButton').forEach((btn,i)=> {
+  btn.addEventListener('click', function() {
+    var idString=btn.id;
+    idString=idString.substring(0,idString.length-7);
+    var selectString="#"+idString+ " .slick-dots button";
+    // console.log(selectString);
+    var myDot=document.querySelectorAll(selectString)[0];
+    myDot.click();
+  });
+});
+
+
+// detailButtons.forEach(btn => {
+//   // item.addEventListener('click', function() {
+//   //   var idString=btn.id;
+//   //   idString=idString.substring(0,idString.length-7);
+//   //   var selectString=idString+ " slick-dots button";
+//   //   var myDot=document.querySelectorAll(selectString)[0];
+//   //   myDot.click();
+//   // });
+//   console.log(btn);
+// });
+// let Click1 = () => {
+// var JP9000_AC= document.querySelectorAll('#JP-9000AC .slick-dots button')[0];
+//   JP9000_AC.click();
+// }
+//
+// let viewdetails = document.getElementById('viewdetails1');
+// viewdetails.addEventListener('click', Click1)
